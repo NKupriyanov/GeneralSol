2aacd98a86c7864a7495aaaa09f89730ee378290
nvkupri <ezulol1996@mail.ru>
2023-08-11 20:05:54 +0300

    create solution

---
+++ Core/Configuration/AppConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Core.Configuration
+{
+    public class AppConfiguration
+    {
+        public static BrowserConfiguration Browser { get; set; } = BindConfiguration<BrowserConfiguration>();
+        private readonly static IConfigurationRoot configurationRoot;
+
+        static AppConfiguration()
+        {
+            configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+        }
+
+        private static T BindConfiguration<T>() where T : IConfiguration, new()
+        {
+            var config = new T();
+            configurationRoot.GetSection(config.SectionName).Bind(config);
+            return config;
+        }
+    }
+}
---
+++ Tests/UI/BaseTest.cs
@@ -0,0 +1,36 @@
+﻿
+using Allure.Commons;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using SeleniumTests.Core.Selenium;
+
+namespace Net.Core.Tests.UI
+{
+    [AllureNUnit]
+    [Parallelizable(ParallelScope.All)]
+    public class BaseTest
+    {
+        private AllureLifecycle allure;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            allure = AllureLifecycle.Instance;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                Screenshot screenshot = ((ITakesScreenshot)Browser.Instance.Driver).GetScreenshot();
+                byte[] bytes = screenshot.AsByteArray;
+                allure.AddAttachment("Screenshot", "image/png", bytes);
+            }
+
+            Browser.Instance.CloseBrowser();
+        }
+    }
+}
