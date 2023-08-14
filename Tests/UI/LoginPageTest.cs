af455fc90ab8ee79c2ff549d9ab369ad7fde9181
Aleksandr <Aleksandr@DESKTOP-5U9IMUU>
2023-08-14 20:16:44 +0300

    add LoginPageTest

---
+++ Tests/UI/LoginPageTest.cs
@@ -0,0 +1,17 @@
+﻿using Net.Core.UI.Pages;
+
+namespace Net.Core.Tests.UI
+{
+    public class LoginPageTest : BaseTest
+    {
+        [Test]
+        public void Login()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                new LoginPage().Login();
+            });
+        }
+    }
+
+}
