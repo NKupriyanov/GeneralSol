4f071272985bfd0ed322e39fb25dfe77ad8b766c
Aleksandr <Aleksandr@DESKTOP-5U9IMUU>
2023-08-14 19:13:33 +0300

    Добавил BasePage

---
+++ UI/Pages/BasePage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using SeleniumTests.Core.Selenium;
+
+
+namespace Net.Core.UI.Pages
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver driver;
+
+        public BasePage()
+        {
+            driver = Browser.Instance.Driver;
+        }
+
+    }
+}
