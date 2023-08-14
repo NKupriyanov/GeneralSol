3b6fbbab332d93474b242a46917bcb4501835266
nvkupri <ezulol1996@mail.ru>
2023-08-14 19:34:10 +0300

    Add login and password

--- Core/Configuration/BrowserConfiguration.cs
+++ Core/Configuration/BrowserConfiguration.cs
@@ -8,6 +8,8 @@ namespace Core.Configuration
 
         public bool Hedless { get; set; }
         public string Type { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
         public int TimeOut { get; set; }
     }
 }
