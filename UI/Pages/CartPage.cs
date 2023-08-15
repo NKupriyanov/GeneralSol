e8491fd208a6c744548eeb3d974dbef49883fb52
nvkupri <ezulol1996@mail.ru>
2023-08-15 19:57:15 +0300

    add CartPage

--- UI/Pages/CartPage.cs
+++ UI/Pages/CartPage.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Net.Core.Core;
+using NUnit.Allure.Attributes;
+using OpenQA.Selenium;
+using SeleniumTests.Core.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,31 @@ namespace Net.Core.UI.Pages
 {
     public class CartPage : BasePage
     {
+        private IWebElement CartSummaryTitle => driver.FindElement(By.XPath("//*[contains(text(),'Shopping-cart summary')]"));
+        private IWebElement ChekOutButton => driver.FindElement(By.XPath("//p[@class='cart_navigation clearfix']//a[@title='Proceed to checkout']"));
+        private IWebElement CartQuantity => driver.FindElement(By.XPath("//span[@class='ajax_cart_quantity']"));
+
+        public CartPage()
+        {
+            WaitHelper.WaitElement(driver, CartSummaryTitle);
+        }
+
+        [AllureStep]
+        public SelectAddressPage ChekOut()
+        {
+            ChekOutButton.Click();
+            LogSession.CurrentSession.Info("ChekOutButton click");
+
+            return new SelectAddressPage();
+        }
+
+        [AllureStep]
+        public string GetProductQuantity()
+        {
+            var count = CartQuantity.Text;
+            LogSession.CurrentSession.Info($"Product Quantity is {count}");
+
+            return count;
+        }
     }
 }
