5d70721c9b28a03a8cc5862501fd200d78722c97
nvkupri <ezulol1996@mail.ru>
2023-08-15 19:59:53 +0300

    add SelectAddressPage

--- UI/Pages/SelectAddressPage.cs
+++ UI/Pages/SelectAddressPage.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Net.Core.Core;
+using NUnit.Allure.Attributes;
+using OpenQA.Selenium;
+using SeleniumTests.Core.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,31 @@ namespace Net.Core.UI.Pages
 {
     public class SelectAddressPage : BasePage
     {
+        private IWebElement AddressPageTitle => driver.FindElement(By.CssSelector("h1:first-child"));
+        private IWebElement ContinueButton => driver.FindElement(By.XPath("//*[@name='processAddress']"));
+        private IWebElement UpdateAddresseButton => driver.FindElement(By.XPath("//*[@class='address_update']//a[@title='Update']"));
+
+        public SelectAddressPage()
+        {
+            WaitHelper.WaitElement(driver, AddressPageTitle);
+        }
+
+        [AllureStep]
+        public NewAddressPage UpdateAddress()
+        {
+            UpdateAddresseButton.Click();
+            LogSession.CurrentSession.Info("UpdateAddresseButton click");
+
+            return new NewAddressPage();
+        }
+
+        [AllureStep]
+        public DeliveryMethodPage Continue()
+        {
+            ContinueButton.Click();
+            LogSession.CurrentSession.Info("ContinueButton click");
+
+            return new DeliveryMethodPage();
+        }
     }
 }
