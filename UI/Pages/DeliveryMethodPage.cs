e3a73fda8794c6926d14eefde2c2c1681d02daf5
nvkupri <ezulol1996@mail.ru>
2023-08-15 20:08:28 +0300

    add DeliveryMethodPage

--- UI/Pages/DeliveryMethodPage.cs
+++ UI/Pages/DeliveryMethodPage.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Net.Core.Core;
+using NUnit.Allure.Attributes;
+using OpenQA.Selenium;
+using SeleniumTests.Core.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,42 @@ namespace Net.Core.UI.Pages
 {
     public class DeliveryMethodPage : BasePage
     {
+        private IWebElement ContinueButton => driver.FindElement(By.Name("processCarrier"));
+        private IWebElement AgreeCheckBox => driver.FindElement(By.Id("uniform-cgv"));
+        private IWebElement StepShiping => driver.FindElement(By.XPath("//*[@class='step_current four']"));
+        private IWebElement ErrorMessage => driver.FindElement(By.XPath("//p[@class='fancybox-error']"));
+
+        public DeliveryMethodPage()
+        {
+            WaitHelper.WaitElement(driver, StepShiping);
+        }
+
+        [AllureStep]
+        public DeliveryMethodPage ContinueWithOutCheckBox()
+        {
+            ContinueButton.Click();
+            LogSession.CurrentSession.Info("ContinueButton click");
+
+            return this;
+        }
+
+        [AllureStep]
+        public string GetErrorMessage()
+        {
+            var message = ErrorMessage.Text;
+            LogSession.CurrentSession.Info($"Error is - {message}");
+
+            return message;
+        }
+
+        [AllureStep]
+        public ChoosePaymentPage ContinueWithCheckBox()
+        {
+            AgreeCheckBox.Click();
+            ContinueButton.Click();
+            LogSession.CurrentSession.Info("AgreeCheckBox and ContinueButton click");
+
+            return new ChoosePaymentPage();
+        }
     }
 }
